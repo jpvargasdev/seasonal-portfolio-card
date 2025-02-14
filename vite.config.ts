5eb41c57f065b745e465d6173b0751c49618aa4d
Juan Vargas <vargasm.jp@gmail.com>
2025-02-14 17:57:54 +0100

    fix: routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "/seasonal-portfolio-card/", // 👈 Set this to match your GitHub repo name
+  base: "/seasonal-portfolio-card/", 
   server: {
     host: "::",
     port: 8080,
